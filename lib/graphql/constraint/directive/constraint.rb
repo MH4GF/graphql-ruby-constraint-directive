3774b8603ae444bb4f6b2149bde159a1c16e41cb
MH4GF <h.miyagi.cnw@gmail.com>
2023-01-03 22:18:41 +0900

    fix: raise error when constraint is attached but owner's type is not String

--- lib/graphql/constraint/directive/constraint.rb
+++ lib/graphql/constraint/directive/constraint.rb
@@ -9,8 +9,8 @@ module GraphQL
         LENGTH = { min_length: :minimum, max_length: :maximum }.freeze
 
         description "validate GraphQL fields"
-        argument :max_length, Int, required: false, description: "validate max length"
-        argument :min_length, Int, required: false, description: "validate min length"
+        argument :max_length, Int, required: false, description: "validate max length for String"
+        argument :min_length, Int, required: false, description: "validate min length for String"
         locations INPUT_FIELD_DEFINITION
 
         def initialize(owner, **arguments)
@@ -22,8 +22,20 @@ module GraphQL
 
         def validation_config
           {
-            length: arguments.keyword_arguments.filter { |key, _| LENGTH.key?(key) }.transform_keys { |key| LENGTH[key] }
+            length: length_config
           }
+        end
+
+        def length_config
+          filtered_args = arguments.keyword_arguments.filter { |key, _| LENGTH.key?(key) }
+
+          if owner.type != GraphQL::Types::String
+            raise ArgumentError, <<~MD
+              #{filtered_args.keys.join(", ")} in @constraint can't be attached to #{owner.graphql_name} because it has to be a String type.
+            MD
+          end
+
+          filtered_args.transform_keys { |key| LENGTH[key] }
         end
       end
     end
